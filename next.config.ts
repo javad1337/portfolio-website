2883de2ea0c23b11c92929df556cd77a8ca0678b
javad <javadxan@gmail.com>
2025-08-07 16:17:35 +0400

    automatic deploy workflow 6

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
-
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    // 👇 NEW — static-export mode
+    output: "export",
+
     webpack(config) {
         config.module.rules.push({
             test: /\.svg$/,
-            use: ["@svgr/webpack"]
+            use: ["@svgr/webpack"],
         });
         return config;
     },
